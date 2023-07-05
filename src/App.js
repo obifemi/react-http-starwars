dd8ba334b2a94dd1f9591e4e92abc14648819262
ali mert inal <mert.inal@gmail.com>
2023-07-05 15:15:46 +0200

    add showing data item by item

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [currentStatus, setStatus] = useState("Click the button to fetch movies")
-  const [movies, setPeople] = useState([])
-  const fetchHandler = async() =>{
-    setStatus("Loading...")
-    const response = await fetch("https://swapi.dev/api/films")
-    const data = await response.json()
-    setPeople(data.results)
-    console.log(movies)
-    
-  }
- 
+  const [currentStatus, setStatus] = useState("Click the button to fetch movies");
+  const [movies, setMovies] = useState([]);
 
+  const fetchMovies = async () => {
+    setStatus("Loading...");
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      const data = await response.json();
+
+      data.results.forEach((film, index) => {
+        setTimeout(() => {
+          setMovies((prevMovies) => [...prevMovies, film]); // Update movies array incrementally
+        }, index * 1000); // Delay each film by 1 second (adjust as needed)
+      });
+
+      setStatus(""); // Clear the status message
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setStatus("Failed to fetch movies");
+    }
+  };
+
+  const fetchHandler = () => {
+    fetchMovies();
+  };
 
   return (
     <React.Fragment>
@@ -23,8 +35,7 @@ function App() {
         <button onClick={fetchHandler}>Fetch Movies</button>
       </section>
       <section>
-         {movies.length === 6 ? <MoviesList movies={movies} /> : <h1>{currentStatus}</h1>   } 
-        
+        {movies.length >= 1 ? <MoviesList movies={movies} /> : <h1>{currentStatus}</h1>}
       </section>
     </React.Fragment>
   );
