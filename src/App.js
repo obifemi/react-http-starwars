897582e46284bdb4d7783909186d5d39e2b555f1
ali mert inal <mert.inal@gmail.com>
2023-07-05 15:00:42 +0200

    add loading

--- src/App.js
+++ src/App.js
@@ -4,37 +4,27 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [currentStatus, setStatus] = useState("Click the button to fetch movies")
   const [movies, setPeople] = useState([])
   const fetchHandler = async() =>{
+    setStatus("Loading...")
     const response = await fetch("https://swapi.dev/api/films")
     const data = await response.json()
     setPeople(data.results)
     console.log(movies)
     
   }
-  fetchHandler()
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+ 
+
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+         {movies.length === 6 ? <MoviesList movies={movies} /> : <h1>{currentStatus}</h1>   } 
+        
       </section>
     </React.Fragment>
   );
